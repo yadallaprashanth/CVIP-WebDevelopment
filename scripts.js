2bef784bf26322d183c1c2904cdc115c104a19e3
YADALLA PRASHANTH <136737942+yadallaprashanth@users.noreply.github.com>
2023-08-27 10:46:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Get the "Get Started" button by its class
+const ctaButton = document.querySelector('.cta-button');
+
+// Add a click event listener to the button
+ctaButton.addEventListener('click', function() {
+  alert('You clicked the Get Started button!');
+});
