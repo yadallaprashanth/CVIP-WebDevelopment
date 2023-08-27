25d9768af30de2c28faef58cc1304abb08f8c6d0
YADALLA PRASHANTH <136737942+yadallaprashanth@users.noreply.github.com>
2023-08-27 15:08:08 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-
-// Get the "Get Started" button by its class
-const ctaButton = document.querySelector('.cta-button');
-
-// Add a click event listener to the button
-ctaButton.addEventListener('click', function() {
-  alert('You clicked the Get Started button!');
-});
